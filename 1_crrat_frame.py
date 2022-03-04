5dd9d73ed92e8cb87c7d568476357b7d9f0d2a14
woong <dndvud123@naver.com>
2022-03-04 18:48:38 +0900

    make title, mainloop

---
+++ 1_crrat_frame.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+root = Tk()
+# 타이틀 이름
+root.title('Images GUI')
+# 창 크기 가로 * 세로
+root.geometry('640x480')
+
+# 창크기 변경 불가(x, y 값)
+#root.resizable(False, False)
+
+root.mainloop()
