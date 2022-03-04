a8f35f1ac5230ecec7cdddc2b7a69dfcf46e0587
woong <dndvud123@naver.com>
2022-03-04 22:31:52 +0900

    practice combobox

--- 8_combobox.py
+++ 8_combobox.py
@@ -7,15 +7,22 @@ root.title('Images GUI')
 # 창 크기 가로 * 세로
 root.geometry('640x480')
 
-
+# 글 쓰기 가능 
 values = [str(i) + '일' for i in range(1, 32)]
 combobox = ttk.Combobox(root, height=5, values= values)
 combobox.pack()
+combobox.set('카드 결제일')
+
+# state='readonly' : 읽기 전용 변경 불가
+readonly_combobox = ttk.Combobox(root, height=5, values= values, state='readonly')
+readonly_combobox.current(0)
+readonly_combobox.pack()
 
 def btncmd():
-    pass
+    print(combobox.get())
+    print(readonly_combobox.get())
 
-btn = Button(root, text='주문', command=btncmd)
+btn = Button(root, text='선택', command=btncmd)
 btn.pack()
 
 
