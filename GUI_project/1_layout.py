1408dadd5ab3ff977b6cd49c6a0af4f0780e06a8
woong <dndvud123@naver.com>
2022-03-09 01:52:46 +0900

    modify file frame and modify option combobox  width

--- GUI_project/1_layout.py
+++ GUI_project/1_layout.py
@@ -8,10 +8,10 @@ root.title('GUI Image')
 
 # 파일 프레임 (파일 추가, 선택삭제)
 file_frame = Frame(root)
-file_frame.pack()
+file_frame.pack(fill='both', expand=True)
 
-btn_add_file = Button(file_frame, padx=5, pady=5, width=12,text='파일 추가')
-btn_sel_del = Button(file_frame, padx=5, pady=5, width=12,text='선택 삭제')
+btn_add_file = Button(file_frame, padx=5, pady=5, width=12, text='파일 추가')
+btn_sel_del = Button(file_frame, padx=5, pady=5, width=12, text='선택 삭제')
 
 btn_add_file.pack(side='left')
 btn_sel_del.pack(side='right')
@@ -44,26 +44,35 @@ option_frame = LabelFrame(root, text='옵션')
 option_frame.pack()
 
 # 가로 넓이
-width_values = ['원본유지', '480 x 720', '1280 x 720']
-width_labal = Label(option_frame, text='가로 넓이')
+# 가로 넓이 레이블
+width_labal = Label(option_frame, text='가로 넓이', padx=10)
 width_labal.grid(row=0, column=0)
-width_cbbox = ttk.Combobox(option_frame, values=width_values, state='readonly')
+
+# 가로 넓이 콤보박스
+width_values = ['원본유지', '1024', '800', '640']
+width_cbbox = ttk.Combobox(option_frame, values=width_values, state='readonly', width=10)
 width_cbbox.current(0)
 width_cbbox.grid(row=0, column=1)
 
 # 간격
-interval_values = ['없음', '있음']
-interval_labal = Label(option_frame, text='간격')
+# 간격 레이블
+interval_labal = Label(option_frame, text='간격', padx=10)
 interval_labal.grid(row=0, column=2)
-interval_cbbox = ttk.Combobox(option_frame, values=interval_values, state='readonly')
+
+# 간격 콤보박스
+interval_values = ['없음', '좁게', '넓게', '보통']
+interval_cbbox = ttk.Combobox(option_frame, values=interval_values, state='readonly', width=10)
 interval_cbbox.current(0)
 interval_cbbox.grid(row=0, column=3)
 
 # 포멧
-format_values = ['PNG', 'IMG']
-format_label = Label(option_frame, text='포멧')
+# 포멧 레이블
+format_label = Label(option_frame, text='포멧', padx=10)
 format_label.grid(row=0, column=4)
-format_cbbox = ttk.Combobox(option_frame, values=format_values, state='readonly')
+
+# 포멧 콤보박스
+format_values = ['PNG', 'JPG', 'BMP']
+format_cbbox = ttk.Combobox(option_frame, values=format_values, state='readonly', width=10)
 format_cbbox.current(0)
 format_cbbox.grid(row=0, column=5)
 
